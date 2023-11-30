d5efb66dbe7ce8a4c47ffb256af5111da7c26075
veera776127 <veeramangalamthulasiram@gmail.com>
2023-11-30 13:56:47 -0500

    change

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='fruit_price_analysis',
+    version='0.2.1',
+    description='Classes for fruit price analysis.',
+    author='Tulsi',
+    author_email='',
+    license='MIT',
+    url='',
+    packages=['fruit_prices'],
+    install_requires=[
+        'matplotlib>=3.0.2',
+        'numpy>=1.15.2',
+        'pandas>=0.23.4',
+        'pandas-datareader>=0.7.0',
+        'seaborn>=0.11.0',
+        'statsmodels>=0.11.1'
+<<<<<<< HEAD
+    ],
+=======
+    ],
+>>>>>>> 78eec57e492b57cf3c9766bab2187140be49bb5b
+)
